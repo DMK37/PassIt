3a94a0204a2b06a0d531b97dd829a61623c2d0de
dima <dimadmk9991@gmail.com>
2024-09-23 20:07:12 +0200

    PassIt: Initial commit

---
+++ userservice/api/server.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"passit/internal/models"
+
+	"github.com/gorilla/mux"
+)
+
+type Server struct {
+	
+}
+
+func (s *Server) Start() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/users", s.createUser).Methods(http.MethodPost)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+        slog.Error("could not start server", "error", err.Error())
+	}
+}
+
+func (s *Server) createUser(w http.ResponseWriter, r *http.Request) {
+
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+        slog.Error("could not decode user", "error", err.Error())
+		http.Error(w, "could not decode user", http.StatusBadRequest)
+		return
+	}
+
+	// save user
+	fmt.Println("user", user)
+	w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(user)
+}
---
+++ userservice/cmd/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	s := api.Server{}
+	s.Start()
+}
